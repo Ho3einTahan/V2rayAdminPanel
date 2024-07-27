4a111ef5f239b5c1fcd322e51de892bdd037f961
MaxBased <hoseintahan84@gmail.com>
2024-07-28 00:55:47 +0330

    Update        main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import { NotFoundExceptionFilter } from './not-found.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+  
+  app.useGlobalFilters(new NotFoundExceptionFilter());
+
+ 
   await app.listen(3000);
+
 }
+
 bootstrap();
