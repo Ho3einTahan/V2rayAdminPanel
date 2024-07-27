164349260b112fe66c240b654367187011733606
MaxBased <hoseintahan84@gmail.com>
2024-07-28 00:57:03 +0330

    add                  not-found-filter.ts

---
+++ src/not-found.filter.ts
@@ -0,0 +1,17 @@
+import { ExceptionFilter, Catch, NotFoundException, HttpStatus, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(NotFoundException)
+export class NotFoundExceptionFilter implements ExceptionFilter {
+    catch(exception: NotFoundException, host: ArgumentsHost) {
+
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+        const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.NOT_FOUND;
+
+        response.status(status).render('404');
+
+    }
+
+}
