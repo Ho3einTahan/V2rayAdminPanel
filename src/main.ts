9558f66f4ce73a37f1d9c4a29d34cf3dcbeaf608
MaxBased <hoseintahan84@gmail.com>
2024-07-29 01:36:45 +0330

    Update              main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { NotFoundExceptionFilter } from './not-found.filter';
+import session from 'express-session';
+import flash from 'connect-flash';
+import { TypeormStore } from 'connect-typeorm';
+import { DataSource } from 'typeorm';
+import { SessionEntity } from './entities/session.entity';
+import cookieParser from 'cookie-parser';
+
 
 async function bootstrap() {
 
@@ -12,10 +19,26 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
-  
+
   app.useGlobalFilters(new NotFoundExceptionFilter());
 
- 
+  const dataSource = app.get(DataSource);
+  const repository = dataSource.getRepository(SessionEntity);
+
+  app.use(session({
+    store: new TypeormStore({
+      // cleanupLimit: 2,
+      ttl: 86400,
+    }).connect(repository),
+    secret: 'this is a secret key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true, maxAge: 3600000 },
+  }));
+
+  app.use(cookieParser());
+  app.use(flash());
+
   await app.listen(3000);
 
 }
