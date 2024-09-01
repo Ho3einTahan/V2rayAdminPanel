9b30121ce0ba08d638a64fd3e9449cda468f54f8
MaxBased <hoseintahan84@gmail.com>
2024-09-02 01:42:10 +0330

    Update                       user.entity.ts

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,15 +1,32 @@
-import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
-@Unique(['email'])
-export class User {
-    
-  @PrimaryGeneratedColumn()
-  id: number;
+@Entity('user')
+export class UserEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: string;
 
-  @Column()
-  email: string;
+  @Column({ unique: true })
+  phoneNumber: string;
 
-  @Column()
+  @Column({nullable:true})
   password: string;
+
+  @Column()
+  userName: string;
+
+  @Column({ nullable: true })
+  startServiceDate: Date;
+
+  @Column({ nullable: true })
+  endServiceDate: Date;
+
+  @Column({ nullable: true })
+  macAddress: string;
+
+  @Column({ nullable: true })
+  multiUser : string;
+
+  @Column({ default: 'user' })
+  role: string;
 }
