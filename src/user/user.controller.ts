60e2cf6b36490f8bbb99d06c5788554ca088e256
MaxBased <hoseintahan84@gmail.com>
2024-09-02 01:41:03 +0330

    Update               user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,61 +7,22 @@ export class UserController {
     constructor(readonly userService: UserService) {
     }
 
-    @Get('login')
-    @Render('login')
-    showLoginPage(@Req() req: Request) {
-
-        const error = req.flash('error');
-        const success = req.flash('success');
-
-        return { success, error };
+    @Get('add')
+    @Render('add-user')
+    getAddUser() {
     }
 
-    
-    @Post('login')
-    async handleLogin(@Body() body, @Res() res: Response, @Req() req: Request) {
-        const { email, password } = body;
-
-        const user = await this.userService.login(email, password);
-
-        if (user) {
-            // req.session = email; // ذخیره ایمیل در سشن
-
-            req.flash('success', 'ورود شما موفقیت آمیز  بود .');
+    @Post('add')
+    addNewUser(@Body() body,@Res() res:Response,@Req() req:Request) {
+        const { userName, phoneNumber, startServiceDate, endServiceDate, multiUser } = body;
+        try {
+            this.userService.addUser(userName, phoneNumber, '', multiUser, startServiceDate, endServiceDate);
+            req.flash('success','کاربر با موفقیت اضافه شد');
             return res.redirect('/');
+        } catch (e) {
+            req.flash('error','خطایی در اضافه کردن کاربر رخ داد');
+            return res.redirect('/user/add');
         }
-        else {
-            req.flash('error', 'نام کاربری یا کلمه عبور اشتباه است .');
-            return res.redirect('/user/login');
-        }
-
     }
-
-
-    @Get('register')
-    @Render('register')
-    showRegisterPage(@Req() req: Request) {
-        const error = req.flash('error');
-        return { error };
-    }
-
-
-    @Post('register')
-    async handleRegister(@Body() body, @Res() res: Response, @Req() req: Request) {
-
-        const { email, password } = body;
-
-        const user = await this.userService.register(email, password);
-
-        if (user) {
-            req.flash('success', 'کابر یا موفقیت ثبت شد .');
-            res.redirect('/user/login');
-        } else {
-            req.flash('error', 'کاربری با این ایمیل ثبت شده است');
-            res.redirect('/user/register');
-        }
-
-    }
-
 
 }
