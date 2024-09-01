de245d0790a8499447e294b5d041ef70065890a9
MaxBased <hoseintahan84@gmail.com>
2024-09-02 01:49:14 +0330

    Update                      main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,56 @@
-
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import { NotFoundExceptionFilter } from './not-found.filter';
 import session from 'express-session';
 import flash from 'connect-flash';
-import { TypeormStore } from 'connect-typeorm';
-import { DataSource } from 'typeorm';
-import { SessionEntity } from './entities/session.entity';
+import passport from 'passport';
 import cookieParser from 'cookie-parser';
-
+import RedisStore from "connect-redis"
+import {createClient} from "redis"
+import { seedAdmin } from './seed';
 
 async function bootstrap() {
-
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  // تنظیمات استاتیک و ویو
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  app.useGlobalFilters(new NotFoundExceptionFilter());
 
-  const dataSource = app.get(DataSource);
-  const repository = dataSource.getRepository(SessionEntity);
 
-  app.use(session({
-    store: new TypeormStore({
-      // cleanupLimit: 2,
-      ttl: 86400,
-    }).connect(repository),
-    secret: 'this is a secret key',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false, httpOnly: true, maxAge: 3600000 },
-  }));
+// Initialize client.
+let redisClient = createClient()
+redisClient.connect().catch(console.error)
 
+// Initialize store.
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "myapp:",
+  ttl:3600,
+})
+
+  app.use(
+    session({
+      store: redisStore,
+      secret: 'your-secret-key',
+      name:'hosein',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 3600000 }, // زمان اعتبار کوکی
+    }),
+  );
+
+  // استفاده از cookie-parser و flash
   app.use(cookieParser());
   app.use(flash());
+
+  // پیکربندی passport
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  seedAdmin();
 
   await app.listen(3000);
 
