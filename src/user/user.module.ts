d4f8ce855714984055bd0cffcd35e022627062a1
MaxBased <hoseintahan84@gmail.com>
2024-09-05 09:44:47 +0330

    Update                        user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,16 +1,19 @@
-import { Module } from '@nestjs/common';
+// user.module.ts
+import { Module, forwardRef } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from '../entities/user.entity';
-import { SessionEntity } from 'src/entities/session.entity';
-import { AuthService } from 'src/auth/auth.service';
-import { LocalStrategy } from 'src/local.strategy';
+import { UserEntity } from '../entities/user.entity';
+import { AuthModule } from '../auth/auth.module'; // اصلاح مسیر
 import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User,SessionEntity]),PassportModule],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    forwardRef(() => AuthModule), // جلوگیری از وابستگی دایره‌ای
+  ],
   controllers: [UserController],
-  providers: [UserService, AuthService, LocalStrategy],
+  providers: [UserService],
+  exports: [UserService],
 })
-export class UserModule { }
+export class UserModule {}
