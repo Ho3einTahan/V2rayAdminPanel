0b13ec1df2b3d170c5e060ae667969f6bbdf3184
MaxBased <hoseintahan84@gmail.com>
2024-09-08 00:44:32 +0330

    add                           api.service.ts

---
+++ src/api/api.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { V2rayConfigEntity } from 'src/entities/v2ray.config.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ApiService {
+
+  
+
+}
