6a5eef7d4722040d232dcd2374a2664abbb47114
MaxBased <hoseintahan84@gmail.com>
2024-09-08 00:46:49 +0330

    add                      jwt.auth.guard.ts

---
+++ src/common/guards/jwt.auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request, Response } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const response = context.switchToHttp().getResponse<Response>();
+
+    try {
+      // این متد از Passport است و به طور خودکار توکن را از درخواست استخراج و اعتبارسنجی می‌کند
+      const result = await super.canActivate(context) as boolean;
+      return result;
+    } catch (e) {
+      response.redirect('/auth/login');
+      return false;
+    }
+  }
+}
