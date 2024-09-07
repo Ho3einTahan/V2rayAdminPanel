d2f1b632945445d6440fbe8477b509788e375127
MaxBased <hoseintahan84@gmail.com>
2024-09-08 00:44:55 +0330

    Update                            app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,27 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Req, Res, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
+import { UserService } from './user/user.service';
+import { JwtAuthGuard } from './common/guards/jwt.auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService,private readonly userService:UserService) { }
 
   @Get()
-  @Render('product')
-  getIndex() {
-    return { uName: 'hosein', isLogin: false };
+  @Render('index')
+ async getIndex(@Req() req: Request, @Res() res: Response) {
+      
+    const users=await this.userService.getAllUsers();
+
+    const success = req.flash('success');
+
+    return {
+      data: users,
+      success: success,
+    };
+
   }
 
-  @Get('checkout')
-  @Render('checkout')
-  getCheckout() {
-    return {uName:'hosein'};
-  }
 
 }
