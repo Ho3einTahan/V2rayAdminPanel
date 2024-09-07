6bc8ba06d73849a38c1f6c593e40ecd6cb56f535
MaxBased <hoseintahan84@gmail.com>
2024-09-08 00:48:27 +0330

    add                               v2ray.config.entity.ts

---
+++ src/entities/v2ray.config.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('v2rayConfig')
+
+export class V2rayConfigEntity {
+
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column('varchar')
+    address: string;
+     
+    @Column('varchar',{length:12})
+    name:string;
+  
+}
