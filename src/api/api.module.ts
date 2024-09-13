09b26d8c9e2b3253d650fe8c7436fbe6652f8838
MaxBased <hoseintahan84@gmail.com>
2024-09-13 14:14:54 +0330

    Update api.module.ts to add new module import

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -5,11 +5,13 @@ import { V2rayConfigEntity } from 'src/entities/v2ray.config.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { ApiService } from './api.service';
 import { ServersModule } from 'src/servers/servers.module';
+import { PricingService } from 'src/pricing/pricing.service';
+import { PricingModule } from 'src/pricing/pricing.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([V2rayConfigEntity]), AuthModule,ServersModule,
+  imports: [TypeOrmModule.forFeature([V2rayConfigEntity]), AuthModule, ServersModule, PricingModule,
   ],
   providers: [ApiService],
   controllers: [ApiController]
 })
-export class ApiModule {}
+export class ApiModule { }
