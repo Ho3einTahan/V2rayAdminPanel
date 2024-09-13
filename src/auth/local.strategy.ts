c92e6e0c977c87ea05bce30793f95dfa39d23ecc
MaxBased <hoseintahan84@gmail.com>
2024-09-13 14:10:06 +0330

    Update local.strategy.ts to support req.body

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -2,22 +2,23 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from 'src/auth/auth.service';
+import { Request } from 'express';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
+export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
   constructor(private authService: AuthService) {
     super({
       usernameField: 'phoneNumber',
+      passReqToCallback: true,  // اضافه کردن این گزینه برای دسترسی به req
     });
   }
 
-  async validate(phoneNumber: string, password: string, req: Request): Promise<any> {
+  async validate(req: Request, phoneNumber: string, password: string): Promise<any> {
 
-    const user = await this.authService.validateUser(phoneNumber, password);
-    console.log('lalakakak');
-    console.log(user);
+    const macAddress = req.body['macAddress'];
+
+    const user = await this.authService.validateUser(phoneNumber, password, macAddress);
+    
     return user;
-
   }
-
 }
