c84557b135f2c869b1ac4690c0a53d20a20a44b8
MaxBased <hoseintahan84@gmail.com>
2024-09-17 08:40:44 +0330

    Update     app.controller.ts  for code cleanup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,3 @@
 import { Controller, Get, Render, Req, Res, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { Request, Response } from 'express';
-import { UserService } from './user/user.service';
-import { JwtAuthGuard } from './common/guards/jwt.auth.guard';
-
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService,private readonly userService:UserService) { }
-
-  @Get()
-  @Render('index')
- async getIndex(@Req() req: Request, @Res() res: Response) {
-      
-    const users=await this.userService.getAllUsers();
-
-    const success = req.flash('success');
-
-    return {
-      data: users,
-      success: success,
-    };
-
-  }
-
-
-}
+export class AppController {}
